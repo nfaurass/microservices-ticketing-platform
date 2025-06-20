b5b076d7212dc44e23e8c52d2e1dd8d086a94efe
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-06-20 16:32:01 +0100

    update routes with needed guards

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -11,19 +11,42 @@ import {CheckinScannerComponent} from './pages/checkin-scanner/checkin-scanner.c
 import {AuthGuard} from './guards/auth.guard';
 
 export const routes: Routes = [
-  {path: '', component: LandingComponent},
-  {path: 'auth', component: AuthComponent},
-  {path: 'events', component: EventsComponent},
-  {path: 'events/:id', component: EventDetailComponent},
   {
-    path: 'my-tickets',
-    component: MyTicketsComponent,
-    canActivate: [AuthGuard],
-    data: {allowedRoles: ['attendee']}
+    path: '', component: LandingComponent
   },
-  {path: 'organizer-dashboard', component: OrganizerDashboardComponent},
-  {path: 'create-event', component: EventFormComponent},
-  {path: 'edit-event/:id', component: EventFormComponent},
-  {path: 'sales-checkins', component: SalesCheckinsComponent},
-  {path: 'checkin-scanner', component: CheckinScannerComponent},
+  {
+    path: 'auth', component: AuthComponent
+  },
+  {
+    path: 'events', component: EventsComponent,
+    canActivate: [AuthGuard], data: {allowedRoles: ['attendee', 'organizer', 'venue']}
+  },
+  {
+    path: 'events/:id', component: EventDetailComponent,
+    canActivate: [AuthGuard], data: {allowedRoles: ['attendee', 'organizer', 'venue']}
+  },
+  {
+    path: 'my-tickets', component: MyTicketsComponent,
+    canActivate: [AuthGuard], data: {allowedRoles: ['attendee']}
+  },
+  {
+    path: 'organizer-dashboard', component: OrganizerDashboardComponent,
+    canActivate: [AuthGuard], data: {allowedRoles: ['organizer']}
+  },
+  {
+    path: 'create-event', component: EventFormComponent,
+    canActivate: [AuthGuard], data: {allowedRoles: ['organizer']}
+  },
+  {
+    path: 'edit-event/:id', component: EventFormComponent,
+    canActivate: [AuthGuard], data: {allowedRoles: ['organizer']}
+  },
+  {
+    path: 'sales-checkins', component: SalesCheckinsComponent,
+    canActivate: [AuthGuard], data: {allowedRoles: ['organizer']}
+  },
+  {
+    path: 'checkin-scanner', component: CheckinScannerComponent,
+    canActivate: [AuthGuard], data: {allowedRoles: ['venue']}
+  },
 ];
