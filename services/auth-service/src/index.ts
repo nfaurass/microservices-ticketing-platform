8e5d3c009cc3c084ff0d72f989af4d0e5e7cc24f
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-06-20 15:42:44 +0100

    add cors to auth-service

--- services/auth-service/src/index.ts
+++ services/auth-service/src/index.ts
@@ -2,10 +2,12 @@ import express, {Request, Response} from 'express';
 import {authRouter} from "./routes/auth";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors({}));
 app.use(express.json());
 app.use(authRouter);
 
