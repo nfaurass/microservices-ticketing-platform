d561e37293a43672ea0da11303d8390abe36d403
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-06-20 17:25:46 +0100

    add basic events service to app

---
+++ frontend/src/app/events.service.ts
@@ -0,0 +1,40 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+export interface TicketType {
+  name: string;
+  price: number;
+  available: number;
+  description?: string;
+}
+
+export interface Ticket {
+  _id: string;
+  eventId: string;
+  ticketTypes: TicketType[];
+}
+
+export interface Event {
+  _id: string;
+  title: string;
+  date: string;
+  venue: string;
+  description: string;
+  imageUrl: string;
+  ticketTypes: Ticket[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+  private readonly API_URL = 'http://localhost:3002/api/events';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getEvents(): Observable<Event[]> {
+    return this.httpClient.get<Event[]>(this.API_URL + "/get");
+  }
+}
