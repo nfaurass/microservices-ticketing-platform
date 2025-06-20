eac9c9cd38f0bb58d370db741a9edeec44a5c766
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-06-20 15:35:08 +0100

    update auth-service user to include type

--- services/auth-service/src/models/Auth.ts
+++ services/auth-service/src/models/Auth.ts
@@ -3,12 +3,14 @@ import mongoose from 'mongoose';
 export interface LoginUserAttrs {
     email: string;
     password: string;
+    type: string;
 }
 
 export interface RegisterUserAttrs {
     name: string;
     email: string;
     password: string;
+    type: string;
 }
 
 export interface UserAttrs {
@@ -16,11 +18,13 @@ export interface UserAttrs {
     name: string;
     email: string;
     password: string;
+    type: string;
 }
 
 export interface UserHeadersAttrs {
     email: string;
     userId: string;
+    type: string;
 }
 
 const authSchema = new mongoose.Schema(
@@ -28,6 +32,11 @@ const authSchema = new mongoose.Schema(
         name: {type: String, required: true},
         email: {type: String, required: true},
         password: {type: String, required: true},
+        type: {
+            type: String,
+            enum: ['organizer', 'attendee', 'staff'],
+            default: 'attendee', required: true
+        },
     },
     {timestamps: true}
 );
--- services/auth-service/src/routes/auth.ts
+++ services/auth-service/src/routes/auth.ts
@@ -7,9 +7,9 @@ import {requireAuth} from "../auth.middleware";
 const router = express.Router();
 
 router.post('/api/auth/login', async (req, res) => {
-    const {email, password} = req.body as LoginUserAttrs;
+    const {email, password, type} = req.body as LoginUserAttrs;
     try {
-        const user = await Auth.findOne({email});
+        const user = await Auth.findOne({email, type: type ?? "attendee"});
         if (!user) {
             res.status(401).json({message: 'Authentication Failed'});
             return;
@@ -20,7 +20,7 @@ router.post('/api/auth/login', async (req, res) => {
             return;
         }
         const jwtToken = jwt.sign(
-            {email: user.email, userId: user._id}, process.env.JWT_SECRET!,
+            {email: user.email, userId: user._id, type: user.type}, process.env.JWT_SECRET!,
             {expiresIn: '1h'}
         );
         res.status(200).json({accessToken: jwtToken, userId: user._id});
@@ -30,17 +30,17 @@ router.post('/api/auth/login', async (req, res) => {
 });
 
 router.post('/api/auth/register', async (req, res) => {
-    const {email, password, name} = req.body as RegisterUserAttrs;
+    const {email, password, name, type} = req.body as RegisterUserAttrs;
     try {
-        const userExists = await Auth.exists({email});
+        const userExists = await Auth.exists({email, name, type});
         if (userExists) {
             res.status(400).json({message: 'User already exists'});
             return;
         }
         const hashedPassword = await bcrypt.hash(password, 12);
-        const authUser = await Auth.create({email, password: hashedPassword, name});
+        const authUser = await Auth.create({email, password: hashedPassword, name, type});
         const jwtToken = jwt.sign(
-            {email: authUser.email, userId: authUser._id}, process.env.JWT_SECRET!,
+            {email: authUser.email, userId: authUser._id, type: authUser.type}, process.env.JWT_SECRET!,
             {expiresIn: '1h'}
         );
         res.status(201).json({accessToken: jwtToken, userId: authUser._id});
@@ -51,7 +51,7 @@ router.post('/api/auth/register', async (req, res) => {
 
 router.get('/api/auth/profile', requireAuth, async (req, res) => {
     const user = (req as any).user as UserHeadersAttrs;
-    const data = await Auth.findOne({email: user.email, _id: user.userId});
+    const data = await Auth.findOne({email: user.email, _id: user.userId, type: user.type});
     if (!data) {
         res.status(401).json({message: 'Failed to get profile'});
         return;
