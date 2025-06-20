646a251ab4afb422a165e34c9a2cc89e7907a719
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-06-20 13:54:44 +0100

    update landing page link to auth

--- frontend/src/app/pages/landing/landing.component.ts
+++ frontend/src/app/pages/landing/landing.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './landing.component.html',
+  standalone: true,
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
