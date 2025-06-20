5a700d26e42a6baaa8aa7af5443d3a4ba7b1bbcb
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-06-20 16:22:31 +0100

    add http modules and update route for guard testing

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {ZXingScannerModule} from '@zxing/ngx-scanner';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
