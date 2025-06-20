4de5cafaaeea95b8f811e44bfaf942783d1212d1
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-06-20 17:32:38 +0100

    add get events id route

--- services/event-service/src/routes/event.ts
+++ services/event-service/src/routes/event.ts
@@ -17,6 +17,21 @@ router.get('/api/events/get', async (req, res) => {
     // Simplified
     const events = await Event.find().populate('ticketTypes');
     res.status(200).send(events);
+});
+
+router.get('/api/events/get/:id', async (req, res) => {
+    const {id} = req.params;
+    if (!id) {
+        res.status(401).json({message: 'Invalid id'});
+        return;
+    }
+    try {
+        // Simplified
+        const events = await Event.findOne({_id: id}).populate('ticketTypes');
+        res.status(200).send(events);
+    } catch {
+        res.status(401).json({message: 'Invalid id'});
+    }
 })
 
 export {router as eventRouter};
