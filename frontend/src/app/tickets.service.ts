b173d03c092f4de146b19204e7c145973cd0e463
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-06-21 15:45:10 +0100

    add getMyTickets to tickets service

--- frontend/src/app/tickets.service.ts
+++ frontend/src/app/tickets.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {AuthService} from './auth.service';
+import {PurchasedTicket} from './pages/my-tickets/my-tickets.component';
 
 export interface TicketType {
   _id?: string;
@@ -32,11 +34,17 @@ export interface Event {
 export class TicketService {
   private readonly API_URL = 'http://localhost:3001/api/tickets';
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private authService: AuthService) {
   }
 
   purchaseTicket(eventId: string, ticketType: string, quantity: string): Observable<any> {
     return this.httpClient.post(this.API_URL + `/purchase/${eventId}`, {ticketType, quantity});
+  }
+
+  getMyTickets(): Observable<PurchasedTicket[]> {
+    return this.httpClient.get<PurchasedTicket[]>(this.API_URL + `/get`, {
+      headers: {Authorization: `Bearer ${this.authService.getAccessToken()}`}
+    });
   }
 
 }
