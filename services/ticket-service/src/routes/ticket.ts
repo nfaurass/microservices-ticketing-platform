c0352b39a6266877ef54bc315a0e038adfa2c00d
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-06-21 15:26:08 +0100

    update ticket-service route to update purchasedTickets

--- services/ticket-service/src/routes/ticket.ts
+++ services/ticket-service/src/routes/ticket.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import {Ticket} from '../models/Ticket';
+import {PurchasedTicket, Ticket} from '../models/Ticket';
 import {publishTicketPurchased} from "../events/publisher/TicketPurchasedPublisher";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
@@ -8,16 +9,35 @@ router.post('/api/tickets/purchase/:eventId', async (req, res) => {
     const {eventId} = req.params as { eventId: string };
     const {ticketType, quantity} = req.body;
     if (eventId) {
-        const event = await Ticket.findOne({eventId});
+        const event = await Ticket.findOne({
+            eventId,
+            ticketTypes: {
+                $elemMatch: {
+                    _id: ticketType,
+                    available: {$gte: quantity}
+                }
+            }
+        });
         if (!event) {
-            res.status(401).json({message: 'Invalid event id'});
+            res.status(401).json({message: 'Invalid event / tickets'});
             return;
         }
         event.ticketTypes.map(t => {
             if (t._id == ticketType) t.available = t.available - quantity;
         });
         const result = await event.save();
-        if (result) await publishTicketPurchased({eventId, ticketType, quantity});
+        if (result) {
+            for (let i = 0; i < quantity; i++) {
+                await PurchasedTicket.create({
+                    eventId: new mongoose.Types.ObjectId(eventId),
+                    ticketType: new mongoose.Types.ObjectId(ticketType.toString()),
+                    userId: new mongoose.Types.ObjectId("685588d619e74dae38321654"),
+                    price: event.ticketTypes.filter(t => t._id == ticketType)[0].price * quantity,
+                    qrCode: eventId.substring(0, 6) + ticketType.substring(0, 6) + Math.random().toString().replace(".", "").substring(0, 16),
+                });
+            }
+            await publishTicketPurchased({eventId, ticketType, quantity});
+        }
     }
     res.status(201).send({message: `successfully purchased: ${eventId} - ${ticketType} - ${quantity}`});
 });
