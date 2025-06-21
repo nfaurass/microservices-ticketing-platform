592fb8d0173d2d9d177f06088998aab609d77ee0
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-06-21 14:25:58 +0100

    add tickets service to app

---
+++ frontend/src/app/tickets.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+export interface TicketType {
+  _id?: string;
+  name: string;
+  price: number;
+  available: number;
+  description?: string;
+}
+
+export interface Ticket {
+  _id: string;
+  eventId: string;
+  ticketTypes: TicketType[];
+}
+
+export interface Event {
+  _id: string;
+  title: string;
+  date: string;
+  venue: string;
+  description: string;
+  imageUrl: string;
+  ticketTypes: Ticket[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TicketService {
+  private readonly API_URL = 'http://localhost:3001/api/tickets';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  purchaseTicket(eventId: string, ticketType: string, quantity: string): Observable<any> {
+    return this.httpClient.post(this.API_URL + `/purchase/${eventId}`, {ticketType, quantity});
+  }
+
+}
